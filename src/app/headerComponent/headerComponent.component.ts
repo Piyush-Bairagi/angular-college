ada96bce5d31a4e166c2a53841355271b84f62b2
Piyush-Bairagi <piyushbairagi77@gmail.com>
2024-01-04 00:54:21 +0530

    initial commit

---
+++ src/app/headerComponent/headerComponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'headerComponent',
+    templateUrl: './headerComponent.component.html',
+    styleUrls: [ './headerComponent.component.css' ]
+})
+export class HeaderComponent {
+    
+ }
