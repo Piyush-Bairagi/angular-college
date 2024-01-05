a66cabf2617dfeab64c094e46fdc8ec3aa588687
Piyush-Bairagi <piyushbairagi77@gmail.com>
2024-01-06 00:49:54 +0530

    Angular ngFor

--- src/app/headerComponent/headerComponent.component.ts
+++ src/app/headerComponent/headerComponent.component.ts
@@ -9,8 +9,16 @@ export class HeaderComponent {
     studentName:string = "Piyush";
     inputBoxValue:string = "Bairagi";
     areAllCSEStudent:boolean = true;
+    stream:string = "CSE";
+    studentDetails = [
+        {name: 'Piyush', branch: 'CSE', address: 'Pune'},
+        {name: 'Ayush', branch: 'BCA', address: 'Indore'},
+        {name: 'Aditya', branch: 'IT', address: 'Indore'},
+        {name: 'Shubham', branch: 'CE', address: 'Hyderabad'},
+    ];
 
     addStudent(input:string) {
         this.studentName = input;
+        
     }
  }
