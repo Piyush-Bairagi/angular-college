0687caf62240c2cf18298a3e90a692853b2458ad
Piyush-Bairagi <piyushbairagi77@gmail.com>
2024-01-25 09:54:28 +0530

    Added bootstra components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './headerComponent/headerComponent.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
