611f5029d254e2b1f0fa1a9ceadcd3ade7e7f33d
Piyush-Bairagi <piyushbairagi77@gmail.com>
2024-02-13 09:10:40 +0530

    Custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './headerComponent/headerComponent.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InrToUsdPipe } from './pipes/inr-to-usd.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, InrToUsdPipe
   ],
   imports: [
     BrowserModule, FormsModule, NgbModule
